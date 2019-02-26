9fe0145fdf654a1901528fbb497630f00e7aa0c7
Arsalan Qayum <arsalan@softaksolutions.com>
2019-02-26 17:44:08 +0500

    separate recent job compnent

---
+++ resources/js/components/home/Recent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="section-padding alice-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="section-header section-header-2 section-header-with-right-content">
+                            <h2>Recent Jobs</h2>
+                            <a href="job-listing.html" class="header-right">+ Browse All Jobs</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="job-list">
+                            <div class="thumb">
+                                <a href="#">
+                                    <img src="images/job/company-logo-1.png" class="img-fluid" alt="">
+                                </a>
+                            </div>
+                            <div class="body">
+                                <div class="content">
+                                    <h4><a href="job-details.html">Designer Required</a></h4>
+                                    <div class="info">
+                                        <span class="company"><a href="#"><i data-feather="briefcase"></i>Theoreo</a></span>
+                                        <span class="office-location"><a href="#"><i data-feather="map-pin"></i>New York City</a></span>
+                                        <span class="job-type full-time"><a href="#"><i data-feather="clock"></i>Full Time</a></span>
+                                    </div>
+                                </div>
+                                <div class="more">
+                                    <div class="buttons">
+                                        <a href="#" class="button">Apply Now</a>
+                                        <a href="#" class="favourite"><i data-feather="heart"></i></a>
+                                    </div>
+                                    <p class="deadline">Deadline: Oct 31, 2018</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
--- resources/js/components/home/mainpage.vue
+++ resources/js/components/home/mainpage.vue
@@ -390,49 +390,7 @@
         </div>
         <!-- Category End -->
         <!-- Jobs -->
-        <div class="section-padding alice-bg">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="section-header section-header-2 section-header-with-right-content">
-                            <h2>Recent Jobs</h2>
-                            <a href="job-listing.html" class="header-right">+ Browse All Jobs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="job-list">
-                            <div class="thumb">
-                                <a href="#">
-                                    <img src="images/job/company-logo-1.png" class="img-fluid" alt="">
-                                </a>
-                            </div>
-                            <div class="body">
-                                <div class="content">
-                                    <h4><a href="job-details.html">Designer Required</a></h4>
-                                    <div class="info">
-                                        <span class="company"><a href="#"><i data-feather="briefcase"></i>Theoreo</a></span>
-                                        <span class="office-location"><a href="#"><i data-feather="map-pin"></i>New York City</a></span>
-                                        <span class="job-type full-time"><a href="#"><i data-feather="clock"></i>Full Time</a></span>
-                                    </div>
-                                </div>
-                                <div class="more">
-                                    <div class="buttons">
-                                        <a href="#" class="button">Apply Now</a>
-                                        <a href="#" class="favourite"><i data-feather="heart"></i></a>
-                                    </div>
-                                    <p class="deadline">Deadline: Oct 31, 2018</p>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
+<recent_job></recent_job>
         <!-- Jobs End -->
         <!--top company-->
         <div class="section-padding-top padding-bottom-90">
@@ -662,6 +620,7 @@
 </template>
 <script>
     import carousel from 'vue-owl-carousel';
+    import recentJob from './Recent'
     export default{
         data(){
           return {
@@ -670,6 +629,7 @@
         },
         components:{
             carousel,
+            recent_job:recentJob
         }
     }
 </script>
