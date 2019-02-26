9a7ece198aa10dcc04d1306d28a4cd82590edee6
Arsalan Qayum <arsalan@softaksolutions.com>
2019-02-26 17:56:47 +0500

    home page is devide into small components

---
+++ resources/js/components/home/TopCompany.vue
@@ -0,0 +1,131 @@
+<template>
+    <div>
+        <div class="section-padding-top padding-bottom-90">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="section-header">
+                            <h2>Top Companies</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <carousel class="" :nav="false" :margin="5" :items="5" :autoplay="true" :responsive="{0:{items:1,nav:false},600:{items:3,nav:false},768:{items:5,nav:false}}">
+
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-1.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">Digoin</a></h4>
+                                    <span>Kansas City, Missouri</span>
+                                    <a href="job-listing.html" class="button">4 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-2.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">Orion Ltd.</a></h4>
+                                    <span>Sacramento, California</span>
+                                    <a href="job-listing.html" class="button">2 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-3.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">Realhouse</a></h4>
+                                    <span>London, United Kingdom</span>
+                                    <a href="job-listing.html" class="button">4 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-4.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">BioPro</a></h4>
+                                    <span>Ajax, Ontarioland</span>
+                                    <a href="job-listing.html" class="button">1 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-1.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">Digoin</a></h4>
+                                    <span>Kansas City, Missouri</span>
+                                    <a href="job-listing.html" class="button">4 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-2.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">Orion Ltd.</a></h4>
+                                    <span>Sacramento, California</span>
+                                    <a href="job-listing.html" class="button">2 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-3.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">Realhouse</a></h4>
+                                    <span>London, United Kingdom</span>
+                                    <a href="job-listing.html" class="button">4 Open Positions</a>
+                                </div>
+                            </div>
+                            <div class="company-wrap">
+                                <div class="thumb">
+                                    <a href="#">
+                                        <img src="../../../../public/images/company/company-logo-4.png" class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="body">
+                                    <h4><a href="employer-details.html">BioPro</a></h4>
+                                    <span>Ajax, Ontarioland</span>
+                                    <a href="job-listing.html" class="button">1 Open Positions</a>
+                                </div>
+                            </div>
+                        </carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import carousel from 'vue-owl-carousel'
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        components:{
+            carousel
+        }
+    }
+</script>
--- resources/js/components/home/mainpage.vue
+++ resources/js/components/home/mainpage.vue
@@ -393,234 +393,18 @@
 <recent_job></recent_job>
         <!-- Jobs End -->
         <!--top company-->
-        <div class="section-padding-top padding-bottom-90">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="section-header">
-                            <h2>Top Companies</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <carousel class="" :nav="false" :margin="5" :items="5" :autoplay="true" :responsive="{0:{items:1,nav:false},600:{items:3,nav:false},768:{items:5,nav:false}}">
-
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-1.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">Digoin</a></h4>
-                                    <span>Kansas City, Missouri</span>
-                                    <a href="job-listing.html" class="button">4 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-2.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">Orion Ltd.</a></h4>
-                                    <span>Sacramento, California</span>
-                                    <a href="job-listing.html" class="button">2 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-3.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">Realhouse</a></h4>
-                                    <span>London, United Kingdom</span>
-                                    <a href="job-listing.html" class="button">4 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-4.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">BioPro</a></h4>
-                                    <span>Ajax, Ontarioland</span>
-                                    <a href="job-listing.html" class="button">1 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-1.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">Digoin</a></h4>
-                                    <span>Kansas City, Missouri</span>
-                                    <a href="job-listing.html" class="button">4 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-2.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">Orion Ltd.</a></h4>
-                                    <span>Sacramento, California</span>
-                                    <a href="job-listing.html" class="button">2 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-3.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">Realhouse</a></h4>
-                                    <span>London, United Kingdom</span>
-                                    <a href="job-listing.html" class="button">4 Open Positions</a>
-                                </div>
-                            </div>
-                            <div class="company-wrap">
-                                <div class="thumb">
-                                    <a href="#">
-                                        <img src="../../../../public/images/company/company-logo-4.png" class="img-fluid" alt="">
-                                    </a>
-                                </div>
-                                <div class="body">
-                                    <h4><a href="employer-details.html">BioPro</a></h4>
-                                    <span>Ajax, Ontarioland</span>
-                                    <a href="job-listing.html" class="button">1 Open Positions</a>
-                                </div>
-                            </div>
-                        </carousel>
-                    </div>
-                </div>
-            </div>
-        </div>
+<top_company></top_company>
         <!--top company end-->
         <!-- Footer -->
-        <footer class="footer-bg">
-            <div class="footer-top border-bottom section-padding-top padding-bottom-40">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="footer-logo">
-                                <a href="#">
-                                    <img src="images/footer-logo.png" class="img-fluid" alt="">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="footer-social">
-                                <ul class="social-icons">
-                                    <li><a href="#"><i data-feather="facebook"></i></a></li>
-                                    <li><a href="#"><i data-feather="twitter"></i></a></li>
-                                    <li><a href="#"><i data-feather="linkedin"></i></a></li>
-                                    <li><a href="#"><i data-feather="instagram"></i></a></li>
-                                    <li><a href="#"><i data-feather="youtube"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-widget-wrapper padding-bottom-60 padding-top-80">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-2 col-sm-6">
-                            <div class="footer-widget footer-shortcut-link">
-                                <h4>Information</h4>
-                                <div class="widget-inner">
-                                    <ul>
-                                        <li><a href="#">About Us</a></li>
-                                        <li><a href="#">Contact Us</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Terms &amp; Conditions</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-sm-6">
-                            <div class="footer-widget footer-shortcut-link">
-                                <h4>Job Seekers</h4>
-                                <div class="widget-inner">
-                                    <ul>
-                                        <li><a href="#">Create Account</a></li>
-                                        <li><a href="#">Career Counseling</a></li>
-                                        <li><a href="#">My Oficiona</a></li>
-                                        <li><a href="#">FAQ</a></li>
-                                        <li><a href="#">Video Guides</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-sm-6">
-                            <div class="footer-widget footer-shortcut-link">
-                                <h4>Employers</h4>
-                                <div class="widget-inner">
-                                    <ul>
-                                        <li><a href="#">Create Account</a></li>
-                                        <li><a href="#">Products/Service</a></li>
-                                        <li><a href="#">Post a Job</a></li>
-                                        <li><a href="#">FAQ</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-5 offset-lg-1 col-sm-6">
-                            <div class="footer-widget footer-newsletter">
-                                <h4>Newsletter</h4>
-                                <p>Get e-mail updates about our latest news and Special offers.</p>
-                                <form action="#" class="newsletter-form form-inline">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Email address...">
-                                    </div>
-                                    <button class="btn button primary-bg">Submit<i class="fas fa-caret-right"></i></button>
-                                    <p class="newsletter-error">0 - Please enter a value</p>
-                                    <p class="newsletter-success">Thank you for subscribing!</p>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom-area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <div class="footer-bottom border-top">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <p class="copyright-text">Copyright <a href="#">Oficiona</a> 2018, All right reserved</p>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="back-to-top">
-                                            <a href="#">Back to top<i class="fas fa-angle-up"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <home_footer></home_footer>
         <!-- Footer End -->
     </div>
 </template>
 <script>
-    import carousel from 'vue-owl-carousel';
-    import recentJob from './Recent'
+
+    import recentJob from './Recent';
+    import TopCompany from './TopCompany'
+    import footer from '../SharedUI/Footer';
     export default{
         data(){
           return {
@@ -628,8 +412,9 @@
           }
         },
         components:{
-            carousel,
-            recent_job:recentJob
+            top_company:TopCompany,
+            recent_job:recentJob,
+            home_footer:footer,
         }
     }
 </script>
